ddba4f093af25bcbe95b31949e887706935d041c
OrwRandal <randypichardo1987@gmail.com>
2023-03-26 20:41:41 -0400

    Started on the search bar

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let burger = document.querySelector('#burger')
 let navbarMenu = document.querySelector('#nav-links')
+let search = document.querySelector('#search')
 
-burger.addEventListener('click', () =>{
+burger.addEventListener('click', (event) =>{
     event.preventDefault();
-
     navbarMenu.classList.toggle('is-active');
+})
+
+search.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('test')
 })
 
