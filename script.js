567f17fbd36ee04376ed82d1744d8bf39d97ddce
OrwRandal <randypichardo1987@gmail.com>
2023-03-29 16:37:03 -0400

    finish categories

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ const searchColumns = document.querySelector('#searchColumns');
 const searchStatus = document.querySelector('#searchStatus');
 let modalStatus = document.querySelector('#modal');
 let modalTitle = document.querySelector('#modalTitle');
-let modalInstruction = document.querySelector('#instructionsText')
+let modalInstruction = document.querySelector('#instructionsText');
+let category = document.querySelector('#categoryDrop')
+let categoryColumns = document.querySelector('#categoryColumns');
 
 burger.addEventListener('click', (event) => {
     event.preventDefault();
@@ -134,8 +136,100 @@ modalClose.addEventListener('click', () => {
     modalStatus.classList.remove('is-active');
 })
 
-// fetch('http://www.themealdb.com/api/json/v1/1/categories.php').then(response => response.json())
-// .then(data => console.log(data))
+// category.
+category.addEventListener("change", () => {
+    console.log(category.value);
+    let idNums = [];
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=' + category.value)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            categoryColumns.innerHTML = '';
+            for (let meal of data.meals) {
+                idNums.push(meal.idMeal)
+            }
+            for (let id of idNums) {
+                fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        let columnDiv = document.createElement('div');
+                        columnDiv.classList.add("column", "is-4-tablet", "is-3-desktop");
+                        let cardDiv = document.createElement('div');
+                        cardDiv.classList.add("card");
+                        let cardImageDiv = document.createElement('div');
+                        cardImageDiv.classList.add("card-image", "has-text-centered");
+                        let cardContentDiv = document.createElement('div');
+                        cardContentDiv.classList.add('card-content');
+                        let cardFooter = document.createElement('footer');
+                        cardFooter.classList.add("card-footer");
+                        columnDiv.appendChild(cardDiv);
+                        cardDiv.appendChild(cardImageDiv);
+                        cardDiv.appendChild(cardContentDiv);
+                        cardDiv.appendChild(cardFooter);
+                        let foodImage = document.createElement('img')
+                        foodImage.src = data.meals[0].strMealThumb;
+                        foodImage.alt = "Picture Of Dish";
+                        foodImage.style.objectFit = 'cover';
+                        foodImage.style.maxHeight = '200px';
+                        foodImage.style.width = '100%';
+                        cardImageDiv.appendChild(foodImage);
+                        let dishName = document.createElement('p')
+                        dishName.classList.add('title');
+                        dishName.innerText = data.meals[0].strMeal;
+                        cardContentDiv.appendChild(dishName);
+
+
+                        let instructionP = document.createElement('p');
+                        instructionP.classList.add("card-footer-item");
+                        let instructionA = document.createElement('a');
+                        instructionA.classList.add("has-text-grey");
+                        instructionA.innerText = 'Instructions';
+                        instructionP.appendChild(instructionA);
+                        cardFooter.appendChild(instructionP);
+
+                        if (data.meals[0].strYoutube) {
+                            let videoP = document.createElement('p');
+                            videoP.classList.add("card-footer-item");
+                            let videoA = document.createElement('a');
+                            videoA.classList.add("has-text-grey");
+                            videoA.innerText = 'Youtube';
+                            videoA.href = data.meals[0].strYoutube;
+                            videoA.target = '_blank';
+                            videoP.appendChild(videoA);
+                            cardFooter.appendChild(videoP);
+                        }
+                        categoryColumns.appendChild(columnDiv);
+
+                        instructionA.addEventListener('click', () => {
+                            modalTitle.innerText = data.meals[0].strMeal;
+                            modalInstruction.innerText = data.meals[0].strInstructions;
+                            let ul = document.querySelector('#ingredientsText');
+                            ul.innerHTML = '';
+                            let i = 1;
+                            while (data.meals[0][`strIngredient${i}`] && i <= 20) {
+                                if (data.meals[0][`strIngredient${i}`].length > 1) {
+                                    let li = document.createElement('li');
+                                    let label = document.createElement('label');
+                                    label.classList.add('checkbox');
+                                    let input = document.createElement('input');
+                                    input.type = 'checkbox';
+                                    label.append(input);
+                                    li.append(label);
+                                    li.append(' ' + data.meals[0][`strMeasure${i}`] + ' ' + data.meals[0][`strIngredient${i}`] + ' ')
+                                    ul.append(li);
+                                }
+                                i++;
+                            }
+                            modalStatus.classList.add('is-active');
+                        })
+                    })
+            }
+        })
+})
+
+
+
 // fetch('http://www.themealdb.com/api/json/v1/1/lookup.php?i=52965').then(response => response.json())
 // .then(data => console.log(data))
 //52965
